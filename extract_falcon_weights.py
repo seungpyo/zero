f655645a5c0e8d943035e9471fc72eedc2da9184
Seungpyo Hong <spkbk98@gmail.com>
2023-09-24 03:29:54 +0900

    converted falcon weights

--- extract_falcon_weights.py
+++ extract_falcon_weights.py
@@ -4,6 +4,7 @@ from time import time
 import torch
 import ctypes
 from zero import zero, ZeroDiskObject, ZeroTensor
+from tqdm import tqdm
 
 hf_cache_dir = os.path.join(str(Path.home()), '.cache', 'huggingface', 'hub')
 model_name = "models--tiiuae--falcon-7b"
@@ -26,27 +27,34 @@ torch_dtype_to_zero_dtype = {
 
 if __name__ == "__main__":
     num_tensors = 0
+    print("Counting tensors...")
     for bin_filename in bin_filenames:
         state_dict = torch.load(bin_filename, map_location=torch.device('cpu'))
         num_tensors += len(state_dict)
-
+    print(f"Found {num_tensors} tensors in {len(bin_filenames)} files")
+    pbar = tqdm(total=num_tensors)
     with open(f"{model_name}.zero", "wb") as f:
         fd = f.fileno()
         f.write(num_tensors.to_bytes(4, byteorder='little'))
-        data_offset = (ctypes.sizeof(ZeroDiskObject) - ctypes.sizeof(ctypes.c_void_p)) * num_tensors + ctypes.sizeof(ctypes.c_uint32)
-        for bin_filename in bin_filenames:
-            state_dict = torch.load(bin_filename, map_location=torch.device('cpu'))
-            disk_objects = [ZeroDiskObject() for _ in state_dict]
-            for i, (k, v) in enumerate(state_dict.items()):
-                print(f"NAME: {k}\tSHAPE: {v.shape}")
-                is_bfloat16 = v.dtype == torch.bfloat16
-                if is_bfloat16:
-                    print(f"Converting {k} from bfloat16 to float32")
-                    v = v.float()
-                b = v.numpy().tobytes()
-                if is_bfloat16:
-                    b = bytes([a for i, a in enumerate(b) if (i // 2) % 2 == 1])
-                zt = ZeroTensor.from_tensor(v, k)
+        f.seek(0)
+        for i, bin_filename in enumerate(bin_filenames):
+            pbar.set_description(f"Loading file {i + 1}/{len(bin_filenames)}")
+            tensors = torch.load(bin_filename, map_location=torch.device('cpu'))
+            pbar.set_description(f"Converting file {i + 1}/{len(bin_filenames)}")
+            zdos = [ZeroDiskObject() for _ in tensors]
+            header_offset = 4
+            offset = (ctypes.sizeof(ZeroDiskObject) - ctypes.sizeof(ctypes.c_void_p)) * num_tensors + ctypes.sizeof(ctypes.c_uint32)
+            for i, (name, tensor) in enumerate(tensors.items()):
+                zt = ZeroTensor.from_tensor(tensor, name)
+                zero.zero_disk_object_serialize_tensor(ctypes.byref(zdos[i]), ctypes.byref(zt))
+                zdos[i].offset = offset
+                offset += zdos[i].size
+            for i, zdo in enumerate(zdos):
+                header_offset = zero.zero_disk_object_write_header_fd(ctypes.byref(zdos[i]), fd, header_offset)
+                zero.zero_disk_object_write_data_fd(ctypes.byref(zdos[i]), fd, zdo.offset)
+                pbar.update(1)
+
+
 
 
 
